9c58f834dc04bd249556a2d4fea03e80f9a861d3
fatimaalatsha <fatima.mohamada@gmail.com>
2024-04-21 22:41:31 -0400

    Welcome page added

--- CouchTo10K/App.js
+++ CouchTo10K/App.js
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Welcome />
+      
       <StatusBar style="auto" />
     </View>
   );
--- CouchTo10K/FrontEnd/components/Welcome.js
+++ CouchTo10K/FrontEnd/components/Welcome.js
@@ -1,27 +1,76 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import * as Yup from "yup";
+import Formik from "formik";
 
-export default function Welcome() {
-    return (
-      // create a view with a text element inside
+export default function Welcome({ navigation }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-      <View style={styles.container}>
-        <Text>Welcome to the app!</Text>
-        <Button
-          title="Go to the next screen"
-          onPress={() => navigation.navigate("NextScreen")}
-        />
-      </View>
-      //create a button element to navigate to the next screen
-    );
+  // validation schema
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+  });
+
+ 
+  const onChangeText = (name) => {
+    setName(name);
+  };
+
+  const onChangeEmail = (email) => {
+    setEmail(email);
+  };
+
+  const handleSubmit = () => {
+    // Handle form submission here
+    console.log("name:", name);
+    console.log("email:", email);
+
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the app!</Text>
+      <Text>yoooo</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(name) => onChangeText(name)}
+        value={name}
+        placeholder="Enter your name"
+        validationSchema={validationSchema}
+      />
+      <Text>Enter your Email</Text>
+  
+      <TextInput
+        style={styles.input}
+        onChangeText={(email) => onChangeEmail(email)}
+        value={email}
+        placeholder="Enter your email"
+        validationSchema={validationSchema}
+      />
+      <Button title="Submit" onPress={handleSubmit} />
+      <Button
+        title="Go to the next screen"
+        onPress={() => navigation.navigate("NextScreen")}
+      />
+    </View>
+  );
 }
-    
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    width: 200,
+  },
+});
